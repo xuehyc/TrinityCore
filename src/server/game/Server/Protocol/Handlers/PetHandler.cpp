c1cd31cf4f68b09cf733255cf06f8a2dcc8d58ab
LordPsyan <uppp@juno.com>
2011-06-17 06:01:59 -0400

    12394-Argent-Tournament
    Signed-off-by: Aquinius <Aquinius@brekeke.com>

--- src/server/game/Server/Protocol/Handlers/PetHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/PetHandler.cpp
@@ -756,6 +756,14 @@ void WorldSession::HandlePetCastSpellOpcode(WorldPacket& recvPacket)
         sLog->outError("WORLD: unknown PET spell id %i", spellId);
         return;
     }
+    switch(spellId)
+    {
+	    case 64077:
+	    {
+		    _player->CastSpell(caster, spellId, true);
+		    return;
+	    }
+    }
 
     if (spellInfo->StartRecoveryCategory > 0) // Check if spell is affected by GCD
         if (caster->GetTypeId() == TYPEID_UNIT && caster->GetCharmInfo() && caster->GetCharmInfo()->GetGlobalCooldownMgr().HasGlobalCooldown(spellInfo))
