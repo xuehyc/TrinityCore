11459e7f7fba437bf978bf5df43e511c90314211
Martin Weinelt <mweinelt@gmail.com>
2012-03-05 16:40:09 +0100

    Fix minor typo in SQL for UtgardePinnacle

--- sql/castle/2012_03_04_00_world_utgardepinnacle.sql
+++ sql/castle/2012_03_04_00_world_utgardepinnacle.sql
@@ -7,7 +7,7 @@ INSERT INTO `achievement_criteria_data` (`criteria_id`, `type`, `value1`, `value
 DELETE FROM `disables` WHERE `sourceType` = 4 AND `entry` = 7595;
 
 -- remove trigger model for spirit fount
-UPDATE `creature_template` SET `modelid1` = 11686, `modelid2` = 0 WHERE `entry` IN (27339, 30808):
+UPDATE `creature_template` SET `modelid1` = 11686, `modelid2` = 0 WHERE `entry` IN (27339, 30808);
 
 -- correct factions for ymiron encounter (avenging spirits, spirit fountain)
 UPDATE `creature_template` SET `faction_A` = '14', `faction_H` = '14' WHERE `entry` IN (27339,30808, 27386, 30756);
