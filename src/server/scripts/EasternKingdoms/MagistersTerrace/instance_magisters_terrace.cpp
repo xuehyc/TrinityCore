d631ce437659b21ca9109b305b56255b8e5b1313
hexa- <mweinelt@gmail.com>
2012-03-15 16:48:29 +0100

    Scripts/Magister's Terrace: codestyle cleanup

--- src/server/scripts/EasternKingdoms/MagistersTerrace/instance_magisters_terrace.cpp
+++ src/server/scripts/EasternKingdoms/MagistersTerrace/instance_magisters_terrace.cpp
@@ -35,6 +35,25 @@ EndScriptData */
 3  - Kael'thas Sunstrider
 */
 
+enum Creatures
+{
+    NPC_SELIN       = 24723,
+    NPC_DELRISSA    = 24560,
+    NPC_FELCRYSTALS = 24722
+};
+
+enum GameObjects
+{
+    GO_VEXALLUS_DOOR        = 187896,
+    GO_SELIN_DOOR           = 187979,
+    GO_SELIN_ENCOUNTER_DOOR = 188065,
+    GO_DELRISSA_DOOR        = 187770,
+    GO_KAEL_DOOR            = 188064,
+    GO_KAEL_STATUE_1        = 188165,
+    GO_KAEL_STATUE_2        = 188166,
+    GO_ESCAPE_ORB           = 188173
+};
+
 class instance_magisters_terrace : public InstanceMapScript
 {
 public:
@@ -49,7 +68,7 @@ public:
     {
         instance_magisters_terrace_InstanceMapScript(Map* map) : InstanceScript(map) {}
 
-        uint32 m_auiEncounter[MAX_ENCOUNTER];
+        uint32 Encounter[MAX_ENCOUNTER];
         uint32 DelrissaDeathCount;
 
         std::list<uint64> FelCrystals;
@@ -69,7 +88,7 @@ public:
 
         void Initialize()
         {
-            memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
+            memset(&Encounter, 0, sizeof(Encounter));
 
             FelCrystals.clear();
 
@@ -92,7 +111,7 @@ public:
         bool IsEncounterInProgress() const
         {
             for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
-                if (m_auiEncounter[i] == IN_PROGRESS)
+                if (Encounter[i] == IN_PROGRESS)
                     return true;
             return false;
         }
@@ -101,12 +120,18 @@ public:
         {
             switch (identifier)
             {
-                case DATA_SELIN_EVENT:          return m_auiEncounter[0];
-                case DATA_VEXALLUS_EVENT:       return m_auiEncounter[1];
-                case DATA_DELRISSA_EVENT:       return m_auiEncounter[2];
-                case DATA_KAELTHAS_EVENT:       return m_auiEncounter[3];
-                case DATA_DELRISSA_DEATH_COUNT: return DelrissaDeathCount;
-                case DATA_FEL_CRYSTAL_SIZE:     return FelCrystals.size();
+                case DATA_SELIN_EVENT:
+                    return Encounter[0];
+                case DATA_VEXALLUS_EVENT:
+                    return Encounter[1];
+                case DATA_DELRISSA_EVENT:
+                    return Encounter[2];
+                case DATA_KAELTHAS_EVENT:
+                    return Encounter[3];
+                case DATA_DELRISSA_DEATH_COUNT:
+                    return DelrissaDeathCount;
+                case DATA_FEL_CRYSTAL_SIZE:
+                    return FelCrystals.size();
             }
             return 0;
         }
@@ -115,21 +140,24 @@ public:
         {
             switch (identifier)
             {
-                case DATA_SELIN_EVENT:       m_auiEncounter[0] = data;  break;
+                case DATA_SELIN_EVENT:
+                    Encounter[0] = data;
+                    break;
                 case DATA_VEXALLUS_EVENT:
                     if (data == DONE)
                         DoUseDoorOrButton(VexallusDoorGUID);
-                    m_auiEncounter[1] = data;
+                    Encounter[1] = data;
                     break;
                 case DATA_DELRISSA_EVENT:
                     if (data == DONE)
                         DoUseDoorOrButton(DelrissaDoorGUID);
                     if (data == IN_PROGRESS)
                         DelrissaDeathCount = 0;
-                    m_auiEncounter[2] = data;
+                    Encounter[2] = data;
                     break;
-                case DATA_KAELTHAS_EVENT:    m_auiEncounter[3] = data;  break;
-
+                case DATA_KAELTHAS_EVENT:
+                    Encounter[3] = data;
+                    break;
                 case DATA_DELRISSA_DEATH_COUNT:
                     if (data == SPECIAL)
                         ++DelrissaDeathCount;
@@ -143,9 +171,15 @@ public:
         {
             switch (creature->GetEntry())
             {
-                case 24723: SelinGUID = creature->GetGUID(); break;
-                case 24560: DelrissaGUID = creature->GetGUID(); break;
-                case 24722: FelCrystals.push_back(creature->GetGUID()); break;
+                case NPC_SELIN:
+                    SelinGUID = creature->GetGUID();
+                    break;
+                case NPC_DELRISSA:
+                    DelrissaGUID = creature->GetGUID();
+                    break;
+                case NPC_FELCRYSTALS:
+                    FelCrystals.push_back(creature->GetGUID());
+                    break;
             }
         }
 
@@ -153,51 +187,30 @@ public:
         {
             switch (go->GetEntry())
             {
-                case 187896:
+                case GO_VEXALLUS_DOOR:
                     VexallusDoorGUID = go->GetGUID();
-                    if (m_auiEncounter[1] == DONE)
-                        HandleGameObject(VexallusDoorGUID, true, go);
-                    else
-                        HandleGameObject(VexallusDoorGUID, false, go);
-
                     break;
-                //SunwellRaid Gate 02
-                case 187979:
+                case GO_SELIN_DOOR:
                     SelinDoorGUID = go->GetGUID();
-                    if (m_auiEncounter[0] == DONE)
-                        HandleGameObject(SelinDoorGUID, true, go);
-                    else
-                        HandleGameObject(SelinDoorGUID, false, go);
-
                     break;
-                //Assembly Chamber Door
-                case 188065:
+                case GO_SELIN_ENCOUNTER_DOOR:
                     SelinEncounterDoorGUID = go->GetGUID();
-                    if (m_auiEncounter[0] == DONE)
-                        HandleGameObject(SelinEncounterDoorGUID, true, go);
-                    else
-                        HandleGameObject(SelinEncounterDoorGUID, false, go);
-
                     break;
-                case 187770:
+                case GO_DELRISSA_DOOR:
                     DelrissaDoorGUID = go->GetGUID();
-                    if (m_auiEncounter[2] == DONE)
-                        HandleGameObject(DelrissaDoorGUID, true, go);
-                    else
-                        HandleGameObject(DelrissaDoorGUID, false, go);
-
                     break;
-                case 188064:
+                case GO_KAEL_DOOR:
                     KaelDoorGUID = go->GetGUID();
-                    if (m_auiEncounter[3] == DONE)
-                        HandleGameObject(KaelDoorGUID, true, go);
-                    else
-                        HandleGameObject(KaelDoorGUID, false, go);
-
                     break;
-                case 188165:  KaelStatue[0] = go->GetGUID();          break;
-                case 188166:  KaelStatue[1] = go->GetGUID();          break;
-                case 188173:  EscapeOrbGUID = go->GetGUID();          break;
+                case GO_KAEL_STATUE_1:
+                    KaelStatue[0] = go->GetGUID();
+                    break;
+                case GO_KAEL_STATUE_2:
+                    KaelStatue[1] = go->GetGUID();
+                    break;
+                case GO_ESCAPE_ORB:
+                    EscapeOrbGUID = go->GetGUID();
+                    break;
             }
         }
 
@@ -205,16 +218,26 @@ public:
         {
             switch (identifier)
             {
-                case DATA_SELIN:                return SelinGUID;
-                case DATA_DELRISSA:             return DelrissaGUID;
-                case DATA_VEXALLUS_DOOR:        return VexallusDoorGUID;
-                case DATA_SELIN_DOOR:           return SelinDoorGUID;
-                case DATA_SELIN_ENCOUNTER_DOOR: return SelinEncounterDoorGUID;
-                case DATA_DELRISSA_DOOR:        return DelrissaDoorGUID;
-                case DATA_KAEL_DOOR:            return KaelDoorGUID;
-                case DATA_KAEL_STATUE_LEFT:     return KaelStatue[0];
-                case DATA_KAEL_STATUE_RIGHT:    return KaelStatue[1];
-                case DATA_ESCAPE_ORB:           return EscapeOrbGUID;
+                case DATA_SELIN:                
+                    return SelinGUID;
+                case DATA_DELRISSA:             
+                    return DelrissaGUID;
+                case DATA_VEXALLUS_DOOR:        
+                    return VexallusDoorGUID;
+                case DATA_SELIN_DOOR:           
+                    return SelinDoorGUID;
+                case DATA_SELIN_ENCOUNTER_DOOR: 
+                    return SelinEncounterDoorGUID;
+                case DATA_DELRISSA_DOOR:        
+                    return DelrissaDoorGUID;
+                case DATA_KAEL_DOOR:            
+                    return KaelDoorGUID;
+                case DATA_KAEL_STATUE_LEFT:     
+                    return KaelStatue[0];
+                case DATA_KAEL_STATUE_RIGHT:    
+                    return KaelStatue[1];
+                case DATA_ESCAPE_ORB:           
+                    return EscapeOrbGUID;
 
                 case DATA_FEL_CRYSTAL:
                 {
