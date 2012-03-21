28481ce5d9e416f0e88af6f7f7619f370c0aef66
hexa- <mweinelt@gmail.com>
2012-03-21 17:12:55 +0100

    DB/Creatures: Set Ragnaros and Murmur to be immune to Grip Effects

--- sql/castle/2012_03_15_00_world_creature_template.sql
+++ sql/castle/2012_03_15_00_world_creature_template.sql
@@ -1,3 +1,5 @@
 -- Update Encounters Murmur+Ragnaros: Bosses should not be able to move at all (cmp. http://www.wowwiki.com/Murmur#Strategy)
+-- Update 2012/03/21: Also add immunity for Death Grip (Grip effect only, not taunt) to ensure they stick in their spawn place.
 UPDATE creature_template SET unit_flags = unit_flags | 4 WHERE entry IN (18708,20657,11502);
+UPDATE creature_template SET mechanic_immune_mask = mechanic_immune_mask | 32 WHERE entry IN (18708,20657,11502);
 
