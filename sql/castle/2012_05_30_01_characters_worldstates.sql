481b186c79bacf0b9b03580db64b64e4a6ab4519
hexa- <mweinelt@gmail.com>
2012-06-01 00:12:35 +0200

    I'm a moron who can't calculate timestamps

--- sql/castle/2012_05_30_01_characters_worldstates.sql
+++ sql/castle/2012_05_30_01_characters_worldstates.sql
@@ -1,2 +1,2 @@
 -- move arena point distribution to friday 6 pm, beginning June 8th 2012
-UPDATE worldstates SET value = 1339779600 WHERE entry = 20001;
+UPDATE worldstates SET value = 1339776000 WHERE entry = 20001;
