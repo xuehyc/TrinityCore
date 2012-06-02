194173fb9500ffe5c876a5c5fa52b14b00c446f5
DorianGrey <ShadowVampire@web.de>
2012-06-02 21:04:02 +0200

    Typo fix ... may cause errors sometimes / somewhere

--- sql/castle/2012_06_02_00_world_spell_script_names.sql
+++ sql/castle/2012_06_02_00_world_spell_script_names.sql
@@ -6,4 +6,4 @@ DELETE FROM `spell_script_names` WHERE `spell_id`=32205;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (32205, 'spell_gen_place_maghar_battle_standard');
 
 -- Remove spell-cast-requirement, since this gets handled by the script.
-UPDATE `quest_template` SET `RequiredSpellCast1`=0, `RequiredSpellCast2`=0, `RequiredSpellCast3`=0 WHERE `Id`=9910 
+UPDATE `quest_template` SET `RequiredSpellCast1`=0, `RequiredSpellCast2`=0, `RequiredSpellCast3`=0 WHERE `Id`=9910; 
