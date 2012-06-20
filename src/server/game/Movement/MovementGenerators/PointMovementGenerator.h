af8095299500e01fcc97d6e13e77880d6313a9f2
Elron103 <scarymovie87@gmx.de>
2012-06-20 21:53:36 +0200

    Core/Movement: Implement speed changes for PointMovementGenerator (pull requested and accepted by TC)

--- src/server/game/Movement/MovementGenerators/PointMovementGenerator.h
+++ src/server/game/Movement/MovementGenerators/PointMovementGenerator.h
@@ -37,6 +37,8 @@ class PointMovementGenerator
 
         void MovementInform(T &);
 
+        void unitSpeedChanged() { i_recalculateSpeed = true; }
+
         MovementGeneratorType GetMovementGeneratorType() { return POINT_MOTION_TYPE; }
 
         bool GetDestination(float& x, float& y, float& z) const { x=i_x; y=i_y; z=i_z; return true; }
@@ -44,6 +46,7 @@ class PointMovementGenerator
         uint32 id;
         float i_x, i_y, i_z;
         float speed;
+        bool i_recalculateSpeed;
 };
 
 class AssistanceMovementGenerator
