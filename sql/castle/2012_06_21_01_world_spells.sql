ef08d72e552bd85e6f0d9a5f6d923690c178decb
Elron103 <scarymovie87@gmx.de>
2012-06-21 18:30:32 +0200

    Fix SQL

--- sql/castle/2012_06_21_01_world_spells.sql
+++ sql/castle/2012_06_21_01_world_spells.sql
@@ -1,8 +1,8 @@
-DELETE FROM spell_script_names WHERE ABS(spell_id) = 66903;
-INSERT INTO spell_script_names (spell_id, ScriptName) VALUES
+DELETE FROM `spell_script_names` WHERE ABS(`spell_id`) IN (66903, 67680);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (67680, 'spell_eadric_hoj'), -- Eadric's Hammer of Justice
-(66903, 'spell_eadric_hoj'); 
+(66903, 'spell_eadric_hoj');
 
-DELETE FROM spell_linked_spell WHERE spell_trigger = 66905;
-INSERT INTO spell_linked_spell (spell_trigger, spell_effect, type, comment) VALUES
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 66905;
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
 (66905, -66904, 0, 'Unflip HOJ Bar when Hammer was thrown');
