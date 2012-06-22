c93d48a823231e77623a2567b9cf6dd85870779f
hexa- <mweinelt@gmail.com>
2012-06-22 04:25:50 +0200

    DB/Conditions: Implicit Targeting for HOTR in Eadric the Pure Encounter

---
+++ sql/castle/2012_06_22_00_eadric_implicit_targeting.sql
@@ -0,0 +1,4 @@
+-- implicit target for hotr throwback in eadric the pure encounter
+DELETE FROM conditions WHERE SourceTypeOrReferenceId = 13 AND SourceEntry = 66905;
+INSERT INTO conditions (SourceTypeOrReferenceId, SourceGroup, SourceEntry, SourceId, ElseGroup, ConditionTypeOrReferenceId, ConditionTarget, ConditionValue1, ConditionValue2, ConditionValue3, NegativeCondition, ErrorTextId, ScriptName, Comment) VALUES
+(13, 0, 66905, 0, 0, 18, 0, 1, 35119, 0, 0, 0, '', 'Hammer of the Righteous - Implict Target Eadric the Pure');
