61d2a5196cf3d4977c50531826510318c4bd4110
Alex <alex@hmele.com.au>
2012-07-08 18:47:01 +1000

    fixes Solace of the Fallen

---
+++ sql/custom/SolaceoftheFallen.sql
@@ -0,0 +1,5 @@
+#This SQL query fixes Solace of the Fallen
+DELETE FROM `spell_proc_event` WHERE entry=67698;
+DELETE FROM `spell_proc_event` WHERE entry=67752;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES (67698, 0, 0, 0, 0, 0, 16384, 65536, 0, 0, 0);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES (67752, 0, 0, 0, 0, 0, 16384, 65536, 0, 0, 0);
