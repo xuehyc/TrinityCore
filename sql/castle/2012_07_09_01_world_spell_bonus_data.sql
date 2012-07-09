fee33c65e5ac4ee967d11de57464d89e33084f9f
hexa- <mweinelt@gmail.com>
2012-07-09 14:39:44 +0200

    DB/Spells: Fix Bonus Data for Shadow Priest Spells

---
+++ sql/castle/2012_07_09_01_world_spell_bonus_data.sql
@@ -0,0 +1,7 @@
+-- update shadow priest spell power coefficients
+UPDATE `spell_bonus_data` SET `direct_bonus`=0.257 WHERE `entry`= 58381;
+UPDATE `spell_bonus_data` SET `direct_bonus`=0.4286 WHERE `entry`= 8092;
+UPDATE `spell_bonus_data` SET `dot_bonus`=0.1849 WHERE `entry`= 2944;
+UPDATE `spell_bonus_data` SET `dot_bonus`=0.4 WHERE `entry`= 34914;
+UPDATE `spell_bonus_data` SET `direct_bonus`=0.4286 WHERE `entry`= 32379;
+UPDATE `spell_bonus_data` SET `dot_bonus`=0.1829 WHERE `entry`= 589;
