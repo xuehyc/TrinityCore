b8a3812b0606dba7d944de28dc305c55aed92b9d
hexa- <mweinelt@gmail.com>
2012-09-20 18:21:06 +0200

    SQL: Fix target_type for Turkey Marker cast

--- sql/castle/2012_09_20_00_world_smart_scripts.sql
+++ sql/castle/2012_09_20_00_world_smart_scripts.sql
@@ -1,2 +1,2 @@
 -- make turkey marker self cast so it will stack with every cast
-UPDATE smart_scripts SET action_type = 85, action_param2 = 0, target_type = 1 WHERE action_type IN (11, 85) AND action_param1 = 25281;
+UPDATE smart_scripts SET action_type = 85, action_param2 = 0, target_type = 7 WHERE action_type IN (11, 85) AND action_param1 = 25281;
