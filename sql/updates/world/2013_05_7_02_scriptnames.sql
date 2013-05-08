8951e27fb5b4381868090a69852ce489dc279369
Spyder <serge@arkania.net>
2013-05-07 19:54:53 -0700

    Core/Scrips: Firelands (sql support are included now but are not implemented yet)

---
+++ sql/updates/world/2013_05_7_02_scriptnames.sql
@@ -0,0 +1,23 @@
+-- firelands ragnarok
+UPDATE `creature_template` SET `ScriptName`='boss_ragnaros_cata' WHERE `entry`='52409';
+UPDATE `creature_template` SET `ScriptName`='npc_magma_trap' WHERE `entry`='53086';
+UPDATE `creature_template` SET `ScriptName`='npc_engulfing_flame' WHERE `entry`='53485';
+UPDATE `creature_template` SET `ScriptName`='npc_sulfuras_hammer' WHERE `entry`='53420';
+UPDATE `creature_template` SET `ScriptName`='npc_son_of_flame' WHERE `entry`='53140';
+-- Script Boss Alysrazor
+UPDATE `creature_template` SET `ScriptName`='npc_harbinger_of_flame' WHERE `entry`='53793';
+UPDATE `creature_template` SET `ScriptName`='npc_blazing_monstrosity' WHERE `entry`='53786';
+UPDATE `creature_template` SET `ScriptName`='npc_blazing_monstrosity' WHERE `entry`='53791';
+UPDATE `creature_template` SET `ScriptName`='npc_molten_barrage' WHERE `entry`='53914';
+UPDATE `creature_template` SET `ScriptName`='npc_egg_pile' WHERE `entry`='53795';
+-- Script Boss Beth'tilac
+UPDATE `creature_template` SET `ScriptName`='boss_bethtilac' WHERE `entry`='52498';
+UPDATE `creature_template` SET `ScriptName`='npc_cinderweb_spinner' WHERE `entry`='52524';
+-- Script Boss Shannox
+UPDATE `creature_template` SET `ScriptName`='npc_rageface' WHERE `entry`='53695';
+UPDATE `creature_template` SET `ScriptName`='npc_riplimb' WHERE `entry`='42692';
+UPDATE `creature_template` SET `ScriptName`='npc_shannox_spear' WHERE `entry`='53752';
+UPDATE `creature_template` SET `ScriptName`='npc_crystal_trap' WHERE `entry`='53713';
+-- Script Boss Majordomo Staghelm
+UPDATE `creature_template` SET `ScriptName`='boss_majordomus' WHERE `entry`='52571';
+UPDATE `creature_template` SET `ScriptName`='npc_spirit_of_the_flame' WHERE `entry`='52593';
