983d34c5ed21c09976849da566df25a6815b1146
Spyder <serge@arkania.net>
2013-05-31 03:13:44 -0700

    Core/spell: Ring of frost

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -113,6 +113,10 @@ DiminishingGroup GetDiminishingReturnsGroupForSpell(SpellInfo const* spellproto,
             // Dragon's Breath
             else if (spellproto->SpellFamilyFlags[0] & 0x800000)
                 return DIMINISHING_DRAGONS_BREATH;
+            // Ring of Frost "Freeze Effect" according to WoWWiki is categorized under "Disorient"
+            // Placed it under Controlled Root due to DIMINISHING_DISORIENT returning a compile error.
+            if (spellproto->Id == 82691)
+               return DIMINISHING_CONTROLLED_ROOT;				
             break;
         }
         case SPELLFAMILY_WARRIOR:
