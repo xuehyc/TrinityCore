88454a4d938059dc906ea545bd008edc5fdc4a56
Spyder <serge@arkania.net>
2013-10-20 17:50:39 +0200

    * Core/Spells: Fixed spell Goblin's Racial Rocket Barrage.

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -587,6 +587,11 @@ void Spell::EffectSchoolDMG(SpellEffIndex effIndex)
                         apply_direct_bonus = false;
                         break;
                     }
+                    // Rocket Barrage, Goblin racial spell
+                    case 69041:
+                    {
+                        damage = uint32(1 + (0.25f * m_caster->GetTotalAttackPowerValue(BASE_ATTACK)) + (0.429f * m_caster->SpellBaseDamageBonusDone(SPELL_SCHOOL_MASK_FIRE)) + (m_caster->getLevel() * 2) + (m_caster->GetStat(STAT_INTELLECT) * 0.50193f));
+                    }					
                 }
                 break;
             }
