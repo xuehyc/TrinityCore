f1a6f8b89c20f1101f7de334672d1dd665b3e394
gpn39f <frankberg@foni.net>
2014-01-18 08:24:50 +0100

    append quest for paladin dwarf and human using Seal of Righteousness

--- sql/updates/world/2014_01_18_00_misc.sql
+++ sql/updates/world/2014_01_18_00_misc.sql
@@ -1,8 +1,22 @@
 
-
+-- new spell spell_pal_seal_of_righteousness
 set @ENTRY=20154;
 delete from spell_script_names where spell_id=@ENTRY;
 insert into spell_script_names values (@ENTRY,"spell_pal_seal_of_righteousness");
+
+-- http://www.wowhead.com/npc=37087  Jona Ironstock quest chain
+UPDATE quest_template SET PrevQuestId=24473 WHERE Id=24474;
+UPDATE quest_template SET NextQuestId=24487 WHERE Id=24475;
+UPDATE quest_template SET PrevQuestId=24475,NextQuestId=182 WHERE Id=24487;
+
+-- http://www.wowhead.com/npc=37514  Grik'nir's Servant  no loot
+update creature_template set lootid=37514 where entry=37514;
+
+-- paladin dwarf quest 24528 using Seal of Righteousness
+UPDATE quest_template SET ObjectiveText2="Learn Spell: Seal of Righteousness" WHERE Id=24528;
+
+-- paladin human quest 26918 using Seal of Righteousness
+UPDATE quest_template SET RequiredNpcOrGo2=44420,RequiredNpcOrGoCount2=1,ObjectiveText2="Learn Spell: Seal of Righteousness" WHERE Id=26918;
 
 
 
