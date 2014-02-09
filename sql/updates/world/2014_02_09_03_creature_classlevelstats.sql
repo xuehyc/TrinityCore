fa6df8f1eb5e4ab4ccd471c392b48445709a0d9c
D3thw0lf <info@xn--kfgames-5wa.de>
2014-02-09 20:55:10 +0100

    fixes the damage for creatures at level 80!
    it's not perfect and needs more optimization

---
+++ sql/updates/world/2014_02_09_03_creature_classlevelstats.sql
@@ -0,0 +1,101 @@
+-- fixes the damage for creatures at level 80! (it's not perfect and needs more optimization)
+
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=1 AND `level`=80;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=1 AND `level`=81;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=1 AND `level`=82;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=1 AND `level`=83;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=1 AND `level`=84;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=1 AND `level`=85;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=1 AND `level`=86;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=1 AND `level`=87;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=1 AND `level`=88;
+
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=2 AND `level`=80;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=2 AND `level`=81;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=2 AND `level`=82;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=2 AND `level`=83;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=2 AND `level`=84;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=2 AND `level`=85;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=2 AND `level`=86;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=2 AND `level`=87;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=2 AND `level`=88;
+
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=3 AND `level`=80;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=3 AND `level`=81;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=3 AND `level`=82;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=3 AND `level`=83;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=3 AND `level`=84;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=3 AND `level`=85;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=3 AND `level`=86;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=3 AND `level`=87;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=3 AND `level`=88;
+
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=4 AND `level`=80;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=4 AND `level`=81;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=4 AND `level`=82;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=4 AND `level`=83;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=4 AND `level`=84;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=4 AND `level`=85;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=4 AND `level`=86;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=4 AND `level`=87;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=4 AND `level`=88;
+
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=5 AND `level`=80;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=5 AND `level`=81;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=5 AND `level`=82;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=5 AND `level`=83;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=5 AND `level`=84;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=5 AND `level`=85;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=5 AND `level`=86;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=5 AND `level`=87;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=5 AND `level`=88;
+
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=6 AND `level`=80;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=6 AND `level`=81;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=6 AND `level`=82;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=6 AND `level`=83;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=6 AND `level`=84;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=6 AND `level`=85;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=6 AND `level`=86;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=6 AND `level`=87;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=6 AND `level`=88;
+
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=7 AND `level`=80;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=7 AND `level`=81;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=7 AND `level`=82;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=7 AND `level`=83;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=7 AND `level`=84;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=7 AND `level`=85;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=7 AND `level`=86;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=7 AND `level`=87;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=7 AND `level`=88;
+
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=8 AND `level`=80;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=8 AND `level`=81;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=8 AND `level`=82;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=8 AND `level`=83;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=8 AND `level`=84;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=8 AND `level`=85;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=8 AND `level`=86;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=8 AND `level`=87;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=8 AND `level`=88;
+
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=9 AND `level`=80;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=9 AND `level`=81;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=9 AND `level`=82;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=9 AND `level`=83;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=9 AND `level`=84;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=9 AND `level`=85;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=9 AND `level`=86;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=9 AND `level`=87;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=9 AND `level`=88;
+
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=11 AND `level`=80;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=11 AND `level`=81;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=11 AND `level`=82;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=11 AND `level`=83;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=11 AND `level`=84;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=11 AND `level`=85;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=11 AND `level`=86;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=11 AND `level`=87;
+UPDATE `creature_classlevelstats` SET `damage_base`=1707.44, `damage_exp1`=1877.4, `damage_exp2`=1077,4, `damage_exp3`=1057,44, `attackpower`=3120, `rangedattackpower`=1120 WHERE `class`=11 AND `level`=88;
