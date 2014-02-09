3ec6311f67bf9c08d899d77fd310d5b0eb22566a
D3thw0lf <info@xn--kfgames-5wa.de>
2014-02-09 07:03:48 +0100

    fixes the wrong display-id for gameobject with the id #19260
    thankies goes to Paul in the Arkania Forums...

---
+++ sql/updates/world/2014_02_09_01_gameobject.sql
@@ -0,0 +1,22 @@
+-- Fix for the icy-moonwell, the thankies goes to Paul in the Arkania Forums...
+-- this changes the display-id of the object #19260 to 0!
+-- no more ice floes in the openworld, YAY!
+
+
+-- fix for the wrong display-id at object #19260
+UPDATE `gameobject_template` SET `displayid`=0 WHERE entry=19260;
+
+
+-- alternative-version. to use this: only remove the "--" in front of these lines and batch
+-- them on your world-database!
+
+
+--DELETE FROM `gameobject_template`
+--WHERE `entry`=19260;
+
+
+
+
+--
+-- ARKANIANS ARE THE BEST! :)
+--
