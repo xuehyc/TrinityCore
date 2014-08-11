ea242dc5cfc77157ab14ece12f839898c0d77989
gpn39f <frankberg@foni.net>
2014-08-11 14:36:20 +0200

    change spell from apply invis 0 to see invis 0

---
+++ sql/updates/world/2014_08_08_01_world_misc_westfall2.sql
@@ -0,0 +1,9 @@
+
+
+-- http://www.wowhead.com/quest=28562  Hero's Call: Westfall!
+-- http://www.wowhead.com/quest=26378  Hero's Call: Westfall!
+-- http://www.wowhead.com/spell=79489  See Quest Invis Zone 0
+UPDATE quest_template SET SourceSpellId=79489 WHERE Id in (26378,28562);
+
+
+
