cf2e167ce55a44f172a4cab03846958d747f2d91
gpn39f <frankberg@foni.net>
2014-08-22 10:05:30 +0200

    fix quest 24933  'Chicken of the Desert'

---
+++ sql/updates/world/2014_08_22_01_world_quest_24933.sql
@@ -0,0 +1,8 @@
+
+-- http://www.wowhead.com/quest=24933  Chicken of the Desert
+UPDATE creature_template SET modelid2=10903,minlevel=46,unit_class=1,family=25,type=1,type_flags=1,lootid=5427,skinloot=5427,spell1=3150,MovementType=1,InhabitType=1,HoverHeight=1.0000,HealthModifier=1.0000,ManaModifier=1.0000,ManaModifierextra=1.0000,ArmorModifier=1.0000,questItem1=9439,questItem2=52476,questItem3=51778,RegenHealth=1 WHERE entry=5427;
+
+
+
+
+
