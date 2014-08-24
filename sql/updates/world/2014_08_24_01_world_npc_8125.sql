11295cf6ba7977aef0c893379b2b1bc1adf34e1b
gpn39f <frankberg@foni.net>
2014-08-24 11:12:31 +0200

    remove old npc.. new npc already in database on correct place

---
+++ sql/updates/world/2014_08_24_01_world_npc_8125.sql
@@ -0,0 +1,9 @@
+
+-- http://www.wowhead.com/npc=8125	Dirge Quikcleave <Butcher>  
+-- since 4.0.3 cooking vendor on a new place and new template entry
+
+delete from creature where guid=87101 and id=8125; -- Guid by ArkDB
+
+
+
+
