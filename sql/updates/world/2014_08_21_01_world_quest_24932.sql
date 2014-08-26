70065f7f2c7af8e9947d466a1860e4d07b5fafeb
gpn39f <frankberg@foni.net>
2014-08-26 12:45:31 +0200

    fix quest 24932

---
+++ sql/updates/world/2014_08_21_01_world_quest_24932.sql
@@ -0,0 +1,6 @@
+
+-- http://www.wowhead.com/quest=24932  Cutting Losses  5441 5450 5451 5452 5453 5454
+update quest_template set QuestGiverPortrait=482 where Id=24932;
+UPDATE quest_template SET QuestGiverTextWindow="Slain any kind of Hazzali Silithid in the Gaping Chasm",QuestGiverTargetName="Hazzali Silithid" WHERE Id=24932;
+
+
