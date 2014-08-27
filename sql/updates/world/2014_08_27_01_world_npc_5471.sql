08210c7dd89eeeed85addd3c8028e0d61f7a9b80
gpn39f <frankberg@foni.net>
2014-08-27 10:12:34 +0200

    remove ogre from wrong places..  no ogre are located at Southmoon Ruins

---
+++ sql/updates/world/2014_08_27_01_world_npc_5471.sql
@@ -0,0 +1,12 @@
+
+-- http://www.wowhead.com/npc=5471	Dunemaul Ogre
+-- https://www.youtube.com/watch?v=FYaYXDCeBp4
+-- no ogre are located at Southmoon Ruins
+
+delete from creature where id=5471 and guid in 
+(86860,86905,86859,86875,86880,86856,86904,86855,86874,86858,86857,86867,86881,86907,86906,86854); -- GUID by ArkDB
+
+
+
+
+
