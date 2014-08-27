525ab43aa9905548e175557008cf55033699f8f4
gpn39f <frankberg@foni.net>
2014-08-27 10:26:51 +0200

    ogre 5475 is not located at Southmoon Ruins.. removed

--- sql/updates/world/2014_08_27_01_world_npc_5471.sql
+++ sql/updates/world/2014_08_27_01_world_npc_5471.sql
@@ -3,8 +3,7 @@
 -- https://www.youtube.com/watch?v=FYaYXDCeBp4
 -- no ogre are located at Southmoon Ruins
 
-delete from creature where id=5471 and guid in 
-(86860,86905,86859,86875,86880,86856,86904,86855,86874,86858,86857,86867,86881,86907,86906,86854); -- GUID by ArkDB
+delete from creature where id=5471 and guid in (86860,86859,86856,86855,86858,86857,86854); -- GUID by ArkDB
 
 
 
---
+++ sql/updates/world/2014_08_27_01_world_npc_5475.sql
@@ -0,0 +1,11 @@
+
+-- http://www.wowhead.com/npc=5475	Dunemaul Ogre
+-- https://www.youtube.com/watch?v=FYaYXDCeBp4
+-- no ogre are located at Southmoon Ruins
+
+delete from creature where id=5475 and guid in (86905,86875,86880,86904,86874,86867,86881,86907,86906); -- GUID by ArkDB
+
+
+
+
+
