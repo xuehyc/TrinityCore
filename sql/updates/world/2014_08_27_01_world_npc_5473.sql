a74bacec67b12cda7354224739ecd3170caf4f5b
gpn39f <frankberg@foni.net>
2014-08-27 20:27:04 +0200

    remove more ogre there are located at Southmoon Ruins

---
+++ sql/updates/world/2014_08_27_01_world_npc_5473.sql
@@ -0,0 +1,11 @@
+
+-- http://www.wowhead.com/npc=5473	 Ogre
+-- https://www.youtube.com/watch?v=FYaYXDCeBp4
+-- no ogre are located at Southmoon Ruins
+
+delete from creature where id=5473 and guid=86867; -- GUID by ArkDB
+
+
+
+
+
---
+++ sql/updates/world/2014_08_27_01_world_npc_5474.sql
@@ -0,0 +1,11 @@
+
+-- http://www.wowhead.com/npc=5474	 
+-- https://www.youtube.com/watch?v=FYaYXDCeBp4
+-- no ogre are located at Southmoon Ruins
+
+delete from creature where id=5474 and guid in (86875,86880,86874,86867,86881); -- GUID by ArkDB
+
+
+
+
+
