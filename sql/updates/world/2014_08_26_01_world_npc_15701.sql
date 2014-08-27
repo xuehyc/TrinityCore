53f12301bc439e492c2cebc9a1c6f63febe34b31
gpn39f <frankberg@foni.net>
2014-08-27 10:35:34 +0200

    typo fix on table name

--- sql/updates/world/2014_08_26_01_world_npc_15701.sql
+++ sql/updates/world/2014_08_26_01_world_npc_15701.sql
@@ -1,15 +1,15 @@
 
 -- http://www.wowhead.com/npc=15701	Field Marshal Snowfall <War Effort Commander>
--- has active creature_questtaker 
+-- has active creature_questender 
 -- 15701	8795	http://www.wowhead.com/quest=8795	The Alliance Needs Your Help!
 -- 15701	8796	http://www.wowhead.com/quest=8796	The Alliance Needs Your Help!
 -- 15701	8797	http://www.wowhead.com/quest=8797	The Alliance Needs Your Help!
 -- 15701	8846	http://www.wowhead.com/quest=8846	Five Signets for War Supplies 
 -- 15701	10501	http://www.wowhead.com/quest=10501	The Alliance Needs Your Help!
 
--- all are obsolete since 3.0.8, so i remove the active creature_questtaker entry's
+-- all are obsolete since 3.0.8, so i remove the active creature_questender entry's
 
-delete from creature_questtaker where id=15701 and quest in (8795,8796,8797,8846,10501);
+delete from creature_questender where id=15701 and quest in (8795,8796,8797,8846,10501);
 
 
 
