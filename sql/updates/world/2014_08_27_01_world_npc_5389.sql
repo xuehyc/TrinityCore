e47e8decb17ce4c72f0d11d475377ae94adcfdfa
gpn39f <frankberg@foni.net>
2014-08-27 10:12:04 +0200

    this npc of gunstan is only spawned by script..

---
+++ sql/updates/world/2014_08_27_01_world_npc_5389.sql
@@ -0,0 +1,10 @@
+
+-- http://www.wowhead.com/npc=5389	Prospector Gunstan <Explorers' League>
+-- is spawned by script..
+delete from creature where guid=86185 and id=5389; -- GUID by ArkDB
+
+
+
+
+
+
