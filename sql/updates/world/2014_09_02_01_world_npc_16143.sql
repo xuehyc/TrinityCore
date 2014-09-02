e0852e9253e54343b239170224f165b6398854b8
gpn39f <frankberg@foni.net>
2014-09-02 11:35:58 +0200

    remove npc. is spawned by script

---
+++ sql/updates/world/2014_09_02_01_world_npc_16143.sql
@@ -0,0 +1,6 @@
+
+-- http://db.vanillagaming.org/?npc=16143	Shadow of Doom	 
+-- http://db.vanillagaming.org/?quest=9085	Shadows of Doom
+
+-- NPC map is not available. NPC is spawned by a script. 
+delete from creature where id=16143;
