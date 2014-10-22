614f4108f41bc4a0cf367a5aa709ae3a3e1488c9
gpn39f <frankberg56@foni.net>
2014-10-22 19:28:47 +0200

    add update for auth and characters database

---
+++ sql/updates/characters/2014_10_22_01_characters.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE `quest_tracker` (
+  `id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `character_guid` int(10) unsigned NOT NULL DEFAULT '0',
+  `quest_accept_time` datetime NOT NULL,
+  `quest_complete_time` datetime DEFAULT NULL,
+  `quest_abandon_time` datetime DEFAULT NULL,
+  `completed_by_gm` tinyint(1) NOT NULL DEFAULT '0',
+  `core_hash` varchar(120) NOT NULL DEFAULT '0',
+  `core_revision` varchar(120) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
