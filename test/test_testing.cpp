ac8cd76de6d56d1c4316fa8dbd9c5ec02c1e794c
DDuarte <dnpd.dd@gmail.com>
2016-01-17 15:09:04 +0000

    Change BOOST_TEST to BOOST_CHECK
    BOOST_TEST was only added in 1.59 which is not yet available in any PPA

--- test/test_testing.cpp
+++ test/test_testing.cpp
@@ -5,9 +5,9 @@ BOOST_AUTO_TEST_CASE(test_macro_overview)
     namespace tt = boost::test_tools;
     int a = 1;
     int b = 2;
-    BOOST_TEST(a != b - 1);
-    BOOST_TEST(a + 1 < b);
-    BOOST_TEST(b - 1 > a, a << " < " << b - 1 << " does not hold");
-    BOOST_TEST(a == b, tt::bitwise());
-    BOOST_TEST(a + 0.1 == b - 0.8, tt::tolerance(0.01));
+    BOOST_CHECK(a != b - 1);
+    BOOST_CHECK(a + 1 < b);
+    BOOST_CHECK(b - 1 > a, a << " < " << b - 1 << " does not hold");
+    BOOST_CHECK(a == b, tt::bitwise());
+    BOOST_CHECK(a + 0.1 == b - 0.8, tt::tolerance(0.01));
 }
