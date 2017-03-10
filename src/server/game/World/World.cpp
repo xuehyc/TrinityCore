b4f67d727f07a74b30ca078374a56388baa79ffe
ConanHUN <conan_513@hotmail.com>
2017-03-10 16:10:40 +0100

    Custom respawm speed script

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1481,6 +1481,7 @@ void World::LoadConfigSettings(bool reload)
 	m_bool_configs[CONFIG_HURT_IN_REAL_TIME] = sConfigMgr->GetBoolDefault("Custom.HurtInRealTime", false);
 	m_float_configs[CONFIG_ATTACKSPEED_PLAYER] = sConfigMgr->GetFloatDefault("Custom.AttackSpeedForPlayer", 1.0f);
 	m_float_configs[CONFIG_ATTACKSPEED_ALL] = sConfigMgr->GetFloatDefault("Custom.AttackSpeedForMobs", 1.0f);
+	m_float_configs[CONFIG_RESPAWNSPEED] = sConfigMgr->GetFloatDefault("Custom.RespawnSpeed", 1.0f);
 
 	// Honor for elites and guards
 	m_bool_configs[CONFIG_GAIN_HONOR_GUARD] = sConfigMgr->GetBoolDefault("Custom.GainHonorOnGuardKill", false);
