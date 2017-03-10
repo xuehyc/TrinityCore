1fab78866756f68fd70b2ff51f3800f3f0f7d03e
ConanHUN <conan_513@hotmail.com>
2017-03-10 18:55:37 +0100

    Fix default attack speed

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -557,7 +557,7 @@ void Unit::resetAttackTimer(WeaponAttackType type)
 	{
 		if (GetTypeId() == TYPEID_PLAYER || (ToCreature()->GetOwner() && ToCreature()->GetOwner()->GetTypeId() == TYPEID_PLAYER))
 		{
-			m_attackTimer[type] = uint32(GetBaseAttackTime(type) * m_modAttackSpeedPct[type] / 3 / sWorld->getFloatConfig(CONFIG_ATTACKSPEED_PLAYER));
+			m_attackTimer[type] = uint32(GetBaseAttackTime(type) * m_modAttackSpeedPct[type] / 2 / sWorld->getFloatConfig(CONFIG_ATTACKSPEED_PLAYER));
 		}
 		else
 		{
