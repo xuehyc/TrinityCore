88e405baa29956bd3543d75e14a41be3fd10fc60
ConanHUN <conan_513@hotmail.com>
2017-03-14 11:12:47 +0100

    Custom duel reset

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -26,6 +26,7 @@ void AddSC_Boss_Announcer();
 void AddSC_XpWeekend();
 void AddSC_RandomEnchantsScript();
 void AddSC_Dynamic_Resurrections();
+void AddSC_Duel_Reset();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
@@ -41,4 +42,5 @@ void AddCustomScripts()
 	AddSC_XpWeekend();
 	AddSC_RandomEnchantsScript();
 	AddSC_Dynamic_Resurrections();
+	AddSC_Duel_Reset();
 }
