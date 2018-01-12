02a61572070a686bd9423bf668b961656e459a6f
3razar3 <3razar3@gmail.com>
2018-01-12 04:17:29 -0500

    Forcing Dark-iCE / AshamaneCore Merge (because TC are cocksuckers)
    Signed-off-by: 3razar3 <3razar3@gmail.com>

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -17,10 +17,16 @@
 
 // This is where scripts' loading functions should be declared:
 void AddSC_custom_npcs();
+void AddSC_solocraft();
+void AddSC_Player_Boa();
+void AddSC_custom_CongratsOnLevel();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
     AddSC_custom_npcs();
+	AddSC_solocraft();
+	AddSC_Player_Boa();
+	AddSC_custom_CongratsOnLevel();
 }
