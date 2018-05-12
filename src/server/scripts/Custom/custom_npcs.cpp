7299d0d7639e59af88087f5108b3d80c845b6189
John Doe <conan_513@hotmail.com>
2018-05-12 21:32:08 +0200

    Fix dmca merge

--- src/server/scripts/Custom/custom_npcs.cpp
+++ src/server/scripts/Custom/custom_npcs.cpp
@@ -35,7 +35,6 @@ class npc_rate_xp_modifier : public CreatureScript
 
         bool OnGossipHello(Player* player, Creature* creature) override
         {
-<<<<<<< HEAD
             if (sConfigMgr->GetBoolDefault("Custom.XP.Rate.NPC", true))
             {
                 for (uint32 i = 1; i <= MAX_RATE; ++i)
@@ -53,20 +52,6 @@ class npc_rate_xp_modifier : public CreatureScript
             }
             else
                 ChatHandler(player->GetSession()).SendSysMessage("The Custom XP Rate NPC is |cff4CFF00disabled |ron this server.");
-=======
-            for (uint32 i = 1; i <= MAX_RATE; ++i)
-            {
-                if (i == player->GetPersonnalXpRate())
-                    continue;
-
-                if (i == sWorld->getRate(RATE_XP_KILL))
-                    continue;
-
-                std::ostringstream gossipText;
-                gossipText << "Rate x" << i;
-                AddGossipItemFor(player, GOSSIP_ICON_CHAT, gossipText.str(), GOSSIP_SENDER_MAIN, i);
-            }
->>>>>>> 382750f587223f2753642e86a1a680faa877a58e
 
             if (player->GetPersonnalXpRate())
             {
