01b83a132a8e0d31afd7426e827ed215abbce665
Shauren <shauren.trinity@gmail.com>
2020-04-08 13:36:59 +0200

    Core/Items: Fixed learning 2nd and 3rd copies of battle pets from items (not caged pets)

--- src/server/game/Spells/SpellMgr.h
+++ src/server/game/Spells/SpellMgr.h
@@ -37,6 +37,7 @@ class SpellInfo;
 class Player;
 class Unit;
 class ProcEventInfo;
+struct BattlePetSpeciesEntry;
 struct SkillLineAbilityEntry;
 struct SpellEntry;
 struct SpellAuraOptionsEntry;
@@ -702,6 +703,8 @@ class TC_GAME_API SpellMgr
 
         uint32 GetModelForTotem(uint32 spellId, uint8 race) const;
 
+        BattlePetSpeciesEntry const* GetBattlePetSpecies(uint32 spellId) const;
+
     private:
         SpellInfo* _GetSpellInfo(uint32 spellId) { return spellId < GetSpellInfoStoreSize() ?  mSpellInfoMap[spellId] : NULL; }
 
@@ -765,6 +768,7 @@ class TC_GAME_API SpellMgr
         PetDefaultSpellsMap        mPetDefaultSpellsMap;           // only spells not listed in related mPetLevelupSpellMap entry
         SpellInfoMap               mSpellInfoMap;
         SpellTotemModelMap         mSpellTotemModel;
+        std::unordered_map<uint32, BattlePetSpeciesEntry const*> mBattlePets;
 };
 
 #define sSpellMgr SpellMgr::instance()
