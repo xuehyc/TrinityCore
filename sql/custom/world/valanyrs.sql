18ee600f79b6a0a185461b88a8fe473b6448e34a
Winfidonarleyan <dowlandtop@yandex.com>
2020-12-27 08:01:14 +0700

    Revert "Core/Spells: val'anirs proc"
    This reverts commit 9dda9f3ffefce50a8c298c4abbe0ac0ae4fdda0d.

--- sql/custom/world/valanyrs.sql
+++
@@ -1,8 +0,0 @@
---Val'anyr -- http://www.wowhead.com/item=46017/valanyr-hammer-of-ancient-kings#comments:id=705194
-
--- Need convert to `spell_proc` table
-/*
-DELETE FROM `spell_proc_event` WHERE `entry` IN (64411, 64415);
-INSERT INTO `spell_proc_event` VALUES (64411, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-INSERT INTO `spell_proc_event` VALUES (64415, 0, 0, 0, 0, 0, 0, 524288, 0, 0, 45000);
-*/
