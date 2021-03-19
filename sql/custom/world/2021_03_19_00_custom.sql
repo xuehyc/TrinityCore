9b769ffb0d76443da3eb81ec85a49721fbe3dadc
ReyDonovan <gennady.zamalaev@gmail.com>
2021-03-19 20:13:49 +0300

    Core/Battleground: implement Twin Peak`s

---
+++ sql/custom/world/2021_03_19_00_custom.sql
@@ -0,0 +1,6 @@
+UPDATE `battleground_template` SET `MaxLvl` = 110;
+
+UPDATE `battleground_template` SET `MinLvl`= 75, `StartMaxDist`= 75, `Weight`= 1 WHERE `ID`= 120;
+UPDATE `battleground_template` SET `MinLvl`= 75, `StartMaxDist`= 75, `Weight`= 1 WHERE `ID`= 108;
+
+DELETE FROM `gameobject` WHERE `id` IN (179830, 179831, 179785, 179786, 179871, 179904, 179905, 179899, 179907, 179906) AND `map`= 726;
