925696725ce078b802fb4292a7204d41fd88bc2b
acidmanifesto <joshua.lee.betts@gmail.com>
2021-04-06 11:38:17 -0400

    DK portal Workarounds.
    https://www.wowhead.com/quest=13189/warcheif's-blessing
    orgrimmar portal spell that work
    originally spell 58418, however that does not work in game for the portal to orgrimmar spell portal in dk starter area
    
    https://www.wowhead.com/quest=13188/where-kings-walk
    stormwind portal spell that work
    originally spell 58420, however that does not work in game for the portal to stormwind spell portal in dk starter area

---
+++ sql/custom/world/DK Starter Area Portal Fix.sql
@@ -0,0 +1,13 @@
+/*
+https://www.wowhead.com/quest=13189/saurfangs-blessing
+orgrimmar portal spells that work
+originally spell 58418, however that does not work in game for the portal to orgrimmar spell portal in dk starter area
+*/
+UPDATE `gameobject_template` SET `Data0`='94724' WHERE  `entry`=193052;
+
+/*
+https://www.wowhead.com/quest=13188/where-kings-walk
+stormwind portal spell that work
+originally spell 58420, however that does not work in game for the portal to stormwind spell portal in dk starter area
+*/
+UPDATE `gameobject_template` SET `Data0`='85546' WHERE  `entry`=193053;
