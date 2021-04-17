9e133fcfd86463821598d47af81d314f6756c74b
Gwenevere <russianninja@comcast.net>
2021-04-17 18:44:52 -0400

    Update horde_trainers.sql
    updated missing goblins starter trainers

--- sql/custom/world/horde_trainers.sql
+++ sql/custom/world/horde_trainers.sql
@@ -1,4 +1,4 @@
-DELETE FROM `creature_trainer` WHERE `CreatureId` IN (15279, 15284, 15280, 39214, 42618, 15285, 38244, 15513, 15283);
+DELETE FROM `creature_trainer` WHERE `CreatureId` IN (15279, 15280, 15283, 15284, 15285, 15513, 34673, 34693, 38244, 39214, 42618);
 insert into `creature_trainer` values 
 ('15279', '44', '14136', '0'),
 ('15284', '127', '14139', '0'),
@@ -8,11 +8,19 @@ insert into `creature_trainer` values
 ('38244', '17', '10985', '0'),
 ('39214', '15', '11185', '0'),
 ('15513', '15', '14141', '0'),
+('34673', '15', '10675', '0'),
+('34693', '17', '10686', '0'),
 ('15283', '32', '14138', '0');
 
-DELETE FROM `gossip_menu_option` WHERE  `MenuId` IN (11185, 14136, 14139, 14137, 11645, 14140, 10985, 14141, 14138);
+DELETE FROM `gossip_menu_option` WHERE  `MenuId` IN (10675, 10686, 10985, 11185, 11645, 14136, 14137, 14138, 14139, 14140, 14141);
 
 Insert into `gossip_menu_option` values
+('10686','0','3', 'I would like training.','3266','5','16','15600'),
+('10686','1','0','I wish to unlearn my talents.','62295','16','16','15600'),
+('10686','2','0','Learn about Dual Talent Specialization.','33762','20','1','15600'),
+('10675','0','3', 'I am interested in hunter training.','3266','5','16','15600'),
+('10675','1','0','I wish to unlearn my talents.','62295','16','16','15600'),
+('10675','2','0','Learn about Dual Talent Specialization.','33762','20','1','15600'),
 ('11185','0','3', 'I seek training in the ways of the Hunter.','3266','5','16','15600'),
 ('11185','1','0','I wish to unlearn my talents.','62295','16','16','15600'),
 ('11185','2','0','Learn about Dual Talent Specialization.','33762','20','1','15600'),
