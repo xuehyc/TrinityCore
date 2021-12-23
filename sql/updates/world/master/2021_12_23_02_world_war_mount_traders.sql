e4cbb4f1d31602db020532facf45ca9f8dccef0b
MobiusDevelopment <mobius@cyber-wizard.com>
2021-12-23 18:23:42 +0200

    Updated PvP mount traders.

---
+++ sql/updates/world/master/2021_12_23_02_world_war_mount_traders.sql
@@ -0,0 +1,41 @@
+-- Horde Combatant Mount Quartermaster
+DELETE FROM `npc_vendor` WHERE `entry`=73151;
+INSERT INTO `npc_vendor` VALUES (73151, 0, 70910, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73151, 0, 102533, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73151, 0, 116778, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73151, 0, 124540, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73151, 0, 140348, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73151, 0, 140354, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73151, 0, 142235, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73151, 0, 142437, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73151, 0, 143649, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73151, 0, 152869, 0, 0, 5214, 1, '', 0, 0, 21021);
+
+-- Horde War Mount Quartermaster
+DELETE FROM `npc_vendor` WHERE `entry`=12796;
+INSERT INTO `npc_vendor` VALUES (12796, 0, 29466, 0, 0, 5976, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (12796, 0, 29469, 0, 0, 5976, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (12796, 0, 29470, 0, 0, 5976, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (12796, 0, 29472, 0, 0, 5976, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (12796, 0, 34129, 0, 0, 5976, 1, '', 0, 0, 21021);
+
+-- Alliance Combatant Mount Quartermaster
+DELETE FROM `npc_vendor` WHERE `entry`=73190;
+INSERT INTO `npc_vendor` VALUES (73190, 0, 70909, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73190, 0, 102514, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73190, 0, 116777, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73190, 0, 124089, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73190, 0, 140350, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73190, 0, 140353, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73190, 0, 142234, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73190, 0, 142237, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73190, 0, 143648, 0, 0, 5214, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (73190, 0, 152870, 0, 0, 5214, 1, '', 0, 0, 21021);
+
+-- Alliance War Mount Quartermaster
+DELETE FROM `npc_vendor` WHERE `entry`=12783;
+INSERT INTO `npc_vendor` VALUES (12783, 0, 29465, 0, 0, 5976, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (12783, 0, 29467, 0, 0, 5976, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (12783, 0, 29468, 0, 0, 5976, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (12783, 0, 29471, 0, 0, 5976, 1, '', 0, 0, 21021);
+INSERT INTO `npc_vendor` VALUES (12783, 0, 35906, 0, 0, 5976, 1, '', 0, 0, 21021);
