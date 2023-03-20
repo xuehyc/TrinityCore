dff19fa6d5d2ef74b753e2b2767375ec9aa5da22
trickerer <onlysuffering@gmail.com>
2023-03-20 18:28:27 +0700

    NPCBots: disable standard creature respawn mechanism for bots

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -2175,6 +2175,11 @@ void Creature::setDeathState(DeathState s)
 
 void Creature::Respawn(bool force)
 {
+    //npcbot
+    if (IsNPCBot())
+        return;
+    //end npcbot
+
     if (force)
     {
         if (IsAlive())
